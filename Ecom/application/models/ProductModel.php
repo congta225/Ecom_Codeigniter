05c29fdc76ffc708e5c6860bf96e76d06f0cf203
congta225 <congta225@gmail.com>
2023-04-19 16:27:49 +0700

    Code backend phần thêm sửa xóa Product

--- Ecom/application/models/ProductModel.php
+++ Ecom/application/models/ProductModel.php
@@ -1,30 +1,31 @@
 <?php
 class ProductModel extends CI_Model
 {
-	public function insertCategory($data)
+	public function insertProduct($data)
 	{
-		return $this->db->insert('categories', $data);
+		return $this->db->insert('products', $data);
 	}
 
-	public function selectCategory()
+	public function selectAllProduct()
 	{
-		$query = $this->db->get('categories');
+		$query = $this->db->select('categories.title as tendanhmuc, products.*, brands.title as tenthuonghieu')
+		->from('categories')
+		->join('products', 'products.category_id = categories.id')
+		->join('brands', 'brands.id=products.brand_id')
+		->get();
 		return $query->result();
 	}
-
-	public function selectCategoryById($id)
-	{
-		$query = $this->db->get_where('categories', ['id' => $id]);
+	public function selectProductById($id){
+		$query=  $this->db->get_where('products', ['id'=> $id]);
 		return $query->row();
 	}
-
-	public function updateCategory($id, $data)
+	public function updateProduct($id, $data)
 	{
-		return $this->db->update('categories', $data, ['id' => $id]);
+		return $this->db->update('products', $data, ['id' => $id]);
 	}
 
-	public function deleteCategory($id)
+	public function deleteProduct($id)
 	{
-		return $this->db->delete('categories', ['id' => $id]);
+		return $this->db->delete('products', ['id' => $id]);
 	}
 }
--- Ecom/application/views/product/create.php
+++ Ecom/application/views/product/create.php
@@ -3,7 +3,7 @@
 		<div class="card-header">
 			Create Product
 		</div>
-		<a href="<?php echo base_url('product/list') ?>" class="btn btn-success">Danh sách danh mục</a>
+		<a href="<?php echo base_url('product/list') ?>" class="btn btn-success">Danh sách sản phẩm</a>
 
 		<div class="card-body">
 			<?php
@@ -17,11 +17,16 @@
 			<?php
 			}
 			?>
-			<form action="<?php echo base_url('category/store') ?>" method="POST" enctype="multipart/form-data">
+			<form action="<?php echo base_url('product/store') ?>" method="POST" enctype="multipart/form-data">
 				<div class="form-group">
 					<label for="exampleInputEmail1">Title</label>
 					<input type="text" name="title" class="form-control">
 					<?php echo '<span class="text text-danger">' . form_error('title') . '</span>' ?>
+				</div>
+				<div class="form-group">
+					<label for="exampleInputEmail1">Quantity</label>
+					<input type="text" name="quantity" class="form-control">
+					<?php echo '<span class="text text-danger">' . form_error('quantity') . '</span>' ?>
 				</div>
 				<div class="form-group">
 					<label for="exampleInputEmail1">Slug</label>
@@ -45,6 +50,32 @@
 					<select class="form-control" name="status">
 						<option value="1">Active</option>
 						<option value="0">InActive</option>
+					</select>
+				</div>
+				<div class="form-group">
+					<label for="exampleFormControlSelect1">Category</label>
+					<select class="form-control" name="category_id">
+						<?php
+						foreach($category as $key => $cate){
+						?>
+							<option value="<?php echo $cate->id ?>"><?php echo $cate->title ?></option>
+						<?php
+						}
+						?>
+					</select>
+				</div>
+				<div class="form-group">
+					<label for="exampleFormControlSelect1">Brand</label>
+					<select class="form-control" name="brand_id">
+
+					<?php
+						foreach($brand as $key => $bra){
+						?>
+							<option value="<?php echo $bra->id ?>"><?php echo $bra->title ?></option>
+						<?php
+						}
+						?>
+						
 					</select>
 				</div>
 
